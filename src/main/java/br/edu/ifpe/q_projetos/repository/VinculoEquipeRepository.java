83c031ba424a0572140960f236b8d14e3d2b9139
Kira175 <167375077+Kira175@users.noreply.github.com>
2026-05-05 10:40:08 -0300

    Create VinculoEquipeRepository.java

---
+++ src/main/java/br/edu/ifpe/q_projetos/repository/VinculoEquipeRepository.java
@@ -0,0 +1,7 @@
+package br.edu.ifpe.q_projetos.repository;
+
+import br.edu.ifpe.q_projetos.model.VinculoEquipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VinculoEquipeRepository extends JpaRepository<VinculoEquipe, Long> {
+}
