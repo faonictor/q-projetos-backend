0c28b0802e74c68f8d4ebab10d0f3fd603f5e1f5
vdrl1808 <146961621+vdrl1808@users.noreply.github.com>
2026-05-12 22:58:59 -0300

    feat:endpoint exclusivo para coordenador

--- src/main/java/br/edu/ifpe/q_projetos/controller/InteresseController.java
+++ src/main/java/br/edu/ifpe/q_projetos/controller/InteresseController.java
@@ -3,12 +3,13 @@ package br.edu.ifpe.q_projetos.controller;
 import br.edu.ifpe.q_projetos.model.Interesse;
 import br.edu.ifpe.q_projetos.service.InteresseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/interesses")
+@RequestMapping("/api/interesses")
 public class InteresseController {
 
     @Autowired
@@ -22,5 +23,15 @@ public class InteresseController {
     @GetMapping
     public List<Interesse> listarTodos() {
         return service.listarTodos();
+    }
+
+    @GetMapping("/coordenador/{responsavel}")
+    public ResponseEntity<List<Interesse>> listarLeadsCoordenador(
+            @PathVariable String responsavel) {
+
+        List<Interesse> leads =
+                service.listarLeadsPorCoordenador(responsavel);
+
+        return ResponseEntity.ok(leads);
     }
 }
